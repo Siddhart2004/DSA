95796b382bb1f7f732a603ce10860e0a05d6ead7
SIDDHARTH A <90645484+Siddhart2004@users.noreply.github.com>
2026-01-28 23:48:05 +0530

    Update 0112-path-sum.cpp

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -1,21 +1,42 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    bool hasPathSum(TreeNode* root, int targetSum) {
+#include <iostream>
+using namespace std;
+
+struct TreeNode{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x){
+        val=x;
+        left=NULL;
+        right=NULL;
+    }
+};
+bool hasPathSum(TreeNode* root, int targetSum) {
         if(root==NULL) return false;
         if(root->left==NULL&&root->right==NULL) {
             return (root->val==targetSum);
         }
         return (hasPathSum(root->left,targetSum-root->val)||hasPathSum(root->right,targetSum-root->val));
     }
-};
+
+int main(){
+    TreeNode* root1=new TreeNode(5);
+    root1->left=new TreeNode(4);
+    root1->right=new TreeNode(8);
+    root1->left->left=new TreeNode(11);
+    root1->left->left->left=new TreeNode(7);
+    root1->left->left->right=new TreeNode(2);
+    root1->right->left=new TreeNode(13);
+    root1->right->right=new TreeNode(4);
+    root1->right->right->right=new TreeNode(1);
+    int targetSum1=22;
+    if(hasPathSum(root1,targetSum1)) cout<<"root 1: true\n";
+    else cout<<"root 1: false\n";
+
+    TreeNode* root2=new TreeNode(1);
+    root2->left=new TreeNode(2);
+    root2->right=new TreeNode(3);
+    int targetSum2=5;
+    if(hasPathSum(root2,targetSum2)) cout<<"root 2: true";
+    else cout<<"root 2: false";
+}
