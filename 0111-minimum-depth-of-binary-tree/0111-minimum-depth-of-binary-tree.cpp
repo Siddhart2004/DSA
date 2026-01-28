7b1cc3815ddc7d9f4b3106effa5e26d2900d087f
SIDDHARTH A <90645484+Siddhart2004@users.noreply.github.com>
2026-01-28 23:06:07 +0530

    Update 0111-minimum-depth-of-binary-tree.cpp

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
@@ -1,17 +1,17 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    int minDepth(TreeNode* root) {
+#include <iostream>
+using namespace std;
+
+struct TreeNode{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x){
+        val=x;
+        left=NULL;
+        right=NULL;
+    }
+};
+int minDepth(TreeNode* root) {
         if (root==NULL) return 0;
         if(root->left==NULL) {
             return minDepth(root->right)+1;
@@ -26,4 +26,18 @@ public:
         }
         return r+1;
     }
-};
+
+int main(){
+    TreeNode* root1=new TreeNode(3);
+    root1->left=new TreeNode(9);
+    root1->right=new TreeNode(20);
+    root1->right->left=new TreeNode(15);
+    root1->right->right=new TreeNode(7);
+    cout<<"root1: "<<minDepth(root1)<<endl;
+    TreeNode* root2=new TreeNode(2);
+    root2->right=new TreeNode(3);
+    root2->right->right=new TreeNode(4);
+    root2->right->right->right=new TreeNode(5);
+    root2->right->right->right->right=new TreeNode(6);
+    cout<<"root2: "<<minDepth(root2);
+}
