cbc99daf79c805bdb46128b323e18029112a104b
SIDDHARTH A <90645484+Siddhart2004@users.noreply.github.com>
2026-02-03 22:47:44 +0530

    Create Split n into maximum composite numbers.java

---
+++ Greedy Algorithms/Split n into maximum composite numbers.java
@@ -0,0 +1,27 @@
+class Main{
+    static int countComposite(int n){
+        if(n<4) return -1;
+
+        int rem=n%4;
+
+        if(rem==0) return n/4;
+        if(rem==1){
+            if(n<9) return -1;
+            return (n-9)/4+1;
+        }
+        if(rem==2){
+            if(n<6) return -1;
+            return (n-6)/4+1;
+        }
+        if(rem==3){
+            if(n<15) return -1;
+            return (n-15)/4+2;
+        }
+
+        return -1;
+    }
+    public static void main(String[] args){
+        System.out.println(countComposite(90));
+        System.out.println(countComposite(143));
+    }
+}
