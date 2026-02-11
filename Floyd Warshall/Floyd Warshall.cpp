38ea51238c4a34b9894678bca258ec163f3c508e
SIDDHARTH A <90645484+Siddhart2004@users.noreply.github.com>
2026-02-11 23:55:33 +0530

    Add Floyd-Warshall algorithm implementation
    Implement Floyd-Warshall algorithm to find shortest paths in a graph.

---
+++ Floyd Warshall/Floyd Warshall.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void floydWarshall(vector<vector<int>>& dist){
+    int n=dist.size();
+    int INF=100000000;
+    for(int k=0;k<n;k++){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(dist[i][k]!=INF && dist[k][j]!=INF){
+                    dist[i][j]=min(dist[i][j],dist[i][k]+dist[k][j]);
+                }
+            }
+        }
+    }
+}
+
+int main(){
+    int INF=100000000;
+    vector<vector<int>> dist={
+        {0,4,INF,5,INF},
+        {INF,0,1,INF,6},
+        {2,INF,0,3,INF},
+        {INF,INF,1,0,2},
+        {1,INF,INF,4,0}
+    };
+    floydWarshall(dist);
+    for(int i=0;i<dist.size();i++){
+        for(int j=0;j<dist.size();j++){
+            cout<<dist[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
